d5be1e445ca24f05e8b21f9d4793e800356e7aa5
HuangJiangye <1419210357@qq.com>
2025-04-24 23:44:53 +0800

    leetcode6，7

--- algorithm/test.js
+++ algorithm/test.js
@@ -29,15 +29,23 @@
 // a.decrement()
 // a.reset()
 
-let arr=[1,2,3];
-let fn = function plusone(n){
-    return n+1;
-}
-var map = function(arr,fn){
-    for(let i=0;i<arr.length;i++){
-        arr[i]=fn(arr[i]);
-    }
-    return arr;
-};
-console.log(map(arr,fn));
+// let arr=[1,2,3];
+// let fn = function plusone(n){
+//     return n+1;
+// }
+// var map = function(arr,fn){
+//     for(let i=0;i<arr.length;i++){
+//         arr[i]=fn(arr[i]);
+//     }
+//     return arr;
+// };
+// console.log(map(arr,fn));
 
+const arr = ['a', 'b', 'c'];
+
+const result = arr.map((element, index, array) => {
+  console.log(`element: ${element}, index: ${index}, array: [${array}]`);
+  return `${index}-${element}`;
+});
+
+console.log(result); // ["0-a", "1-b", "2-c"]
---
+++ damn/javascript/reduce.md
@@ -0,0 +1,20 @@
+# [reduce](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce)
+
+每一次运行 reducer 会将先前元素的计算结果作为参数传入，最后将其结果汇总为单个返回值。
+
+第一次执行回调函数时，不存在“上一次的计算结果”。如果需要回调函数从数组索引为 0 的元素开始执行，则需要传递初始值。否则，数组索引为 0 的元素将被用作初始值，迭代器将从第二个元素开始执行（即从索引为 1 而不是 0 的位置开始）。
+
+```
+const array1 = [1, 2, 3, 4];
+
+// 0 + 1 + 2 + 3 + 4
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue,
+);
+
+console.log(sumWithInitial);
+// Expected output: 10
+```
+
