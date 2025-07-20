18875d724917b96f70666e9e965b181f0e601590
HuangJiangye <1419210357@qq.com>
2025-07-20 18:43:51 +0800

    720-1

--- diary/promise.all手写.js
+++ diary/promise.all手写.js
@@ -23,3 +23,29 @@ function PromiseAll(promises){
         });
     });
 }
+
+
+
+function PromiseAll(promises){
+    return new Promise((resolve,reject)=>{
+        let total=promises.length;
+        let results=[];
+        let completed=0;
+        if(total===0){
+            resolve(results);
+        }
+        promises.forEach((promise,index)=>{
+            Promise.resolve(promise)
+            .then((value)=>{
+                results[index]=value;
+                completed++;
+                if(total===completed){
+                    resolve(results);
+                }
+            })
+            .catch((err)=>{
+                reject(err);
+            })
+        })
+    })
+}
